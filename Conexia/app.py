3011168fd0f6f0b9a14925e040c255e9bf44cc9a
juanpedrombanchama-maker <juanpedrombanchama@gmail.com>
2025-09-30 16:59:21 +0100

    Update app.py

--- Conexia/app.py
+++ Conexia/app.py
@@ -14,5 +14,10 @@ def update():
 def nivel():
     return jsonify(estado_bateria)
 
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
